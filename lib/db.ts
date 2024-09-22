183df616b54563f2c2a2121ad1515e6f1230c4a4
pragyan8804 <89921612+pragyan8804@users.noreply.github.com>
2024-09-22 14:22:02 +0530

    changed db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,3 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prismaClient = new PrismaClient() 
+// Avoid creating multiple instances of PrismaClient in serverless environments like Vercel
+declare global {
+  // This ensures that the PrismaClient is reused during development and doesn't create multiple instances
+  var prisma: PrismaClient | undefined;
+}
+
+export const prismaClient =
+  global.prisma ||
+  new PrismaClient({
+    log: ["query", "info", "warn", "error"], // Optional: Add logging for development and debugging
+  });
+
+if (process.env.NODE_ENV !== "production") global.prisma = prismaClient;
